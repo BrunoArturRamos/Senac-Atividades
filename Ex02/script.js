8a2da49cf4ba92810e035dbafc659eb9e433e5dd
BrunoArtur <brunoarturr01@gmail.com>
2026-06-11 21:51:16 -0300

    Update script.js

--- Ex02/script.js
+++ Ex02/script.js
@@ -1,8 +1,7 @@
 function cadastro() {
-    Event.preventDefaut()
     let nome = document.getElementById('nome').value; /* busca um evento no html, no caso o pNome do Primeiro nome e guarda no pNome */
     let resultado = document.getElementById('resultado');
-    Window.alert(`${nome} seu cad foi bom`);
+    window.alert(`${nome} seu cad foi bom`);
     resultado.innerHTML= `<p>Sr . ${nome}, seu cadastro foi bom </p>`
-
+    return false
 }
