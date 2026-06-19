891b57d7a8062f76dac1008a88e1b968a42dd006
Kayck otavio <brunoarturr01@gmail.com>
2026-06-19 20:31:19 -0300

    Create script.js

---
+++ ex002/script.js
@@ -0,0 +1,18 @@
+let y = 2;
+let x = 5;
+var resultado = x + y;
+console.log(`a soma de x e y é ${resultado}`)
+var resultado = x - y;
+console.log(`a soma de x e y é ${resultado}`)
+var resultado = x / y;
+console.log(`a soma de x e y é ${resultado}`)
+var resultado = x * y;
+console.log(`a soma de x e y é ${resultado}`)
+var resultado = x % y;
+console.log(`a soma de x e y é ${resultado}`)
+
+var num1 = Number.parseInt(window.prompt('digite numero')) 
+var num2 = Number.parseInt(window.prompt('digite ountro numero')) 
+/* quando é string ele não soma, ele concatena */
+var soma = num1 + num2
+window.alert(`soma deu ${soma}`)
